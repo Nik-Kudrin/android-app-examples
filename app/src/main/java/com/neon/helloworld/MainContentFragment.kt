0a0fc5dbc46a364e3aeb60c5f48fd551871f12d8
neon_home <niki.kudrin@gmail.com>
2020-10-24 11:19:54 +0200

    working with resources

--- app/src/main/java/com/neon/helloworld/MainContentFragment.kt
+++ app/src/main/java/com/neon/helloworld/MainContentFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -23,8 +24,23 @@ class MainContentFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         view.findViewById<Button>(R.id.roll_dice_button).setOnClickListener {
-            view.findViewById<TextView>(R.id.dice_roll_result).text = Random.nextInt(1, 6).toString()
+            val randomNumber = Random.nextInt(1, 7)
+            view.findViewById<TextView>(R.id.dice_roll_result).text = randomNumber.toString()
+            view.findViewById<ImageView>(R.id.dice_image).setImageResource(convertNumberToDiceResource(randomNumber))
         }
     }
+
+    private fun convertNumberToDiceResource(number: Int) =
+            when (number) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6 -> R.drawable.dice_6
+                else -> TODO("Able to show only 6 side dice")
+            }
 }
